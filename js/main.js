76ade91458f0878c613b337ba685c36bed85c9c1
mnubhan <nubhant@gmail.com>
2024-01-21 05:45:01 +0800

    feat: Add calculateAge function and update element classes -Add calculateAge function to compute age from a given date -Add class names to elements displaying year, month, and day results

--- js/main.js
+++ js/main.js
@@ -1,17 +1,54 @@
-
 const inputDay = document.querySelector("#day");
 const inputMonth = document.querySelector("#month");
 const inputYear = document.querySelector("#year");
+const btn = document.querySelector(".button");
+const showResultYears = document.querySelector(".show-result-years");
+const showResultMonths = document.querySelector(".show-result-months");
+const showResultDays = document.querySelector(".show-result-days");
 
-const clearInputValue = (event) => {
-    event.target.value = "";
+const calculateAge = () => {
+  const currentDate = new Date();
+  const birthDate = new Date(
+    Number(inputYear.value),
+    Number(inputMonth.value) - 1,
+    Number(inputDay.value)
+  );
+  let years = currentDate.getFullYear() - birthDate.getFullYear();
+  let months = currentDate.getMonth() - birthDate.getMonth();
+  let days = currentDate.getDate() - birthDate.getDate();
+  console.log({
+    year: Number(inputYear.value),
+    month: Number(inputMonth.value),
+    day: Number(inputDay.value),
+  });
+  console.log({
+    currentMonth: currentDate.getMonth(),
+    birthDateMonth: birthDate.getMonth(),
+  });
+  console.log({ birthDate: birthDate });
+  console.log({ years: years, months: months, days: days });
+  if (months < 0 || (months == 0 && days < 0)) {
+    years--;
+    months += 12;
+    console.log(months);
+  }
+
+  if (days < 0) {
+    months--;
+    let tempDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      0
+    );
+    days += tempDate.getDate();
+  }
+
+  showResultYears.textContent = years;
+  showResultMonths.textContent = months;
+  showResultDays.textContent = days;
+  inputDay.value = "";
+  inputMonth.value = "";
+  inputYear.value = "";
 };
 
-inputDay.addEventListener("click", clearInputValue);
-inputDay.addEventListener("touch", clearInputValue);
-
-inputMonth.addEventListener("click", clearInputValue);
-inputMonth.addEventListener("touch", clearInputValue);
-
-inputYear.addEventListener("click", clearInputValue);
-inputYear.addEventListener("touch", clearInputValue);
+btn.addEventListener("click", calculateAge);
