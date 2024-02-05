642774038171f199e56e52ecc1e0144d1883de79
mnubhan <76425959+mnubhan@users.noreply.github.com>
2024-02-06 00:01:57 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ const showResultYears = document.querySelector(".show-result-years");
 const showResultMonths = document.querySelector(".show-result-months");
 const showResultDays = document.querySelector(".show-result-days");
 const errorMessageInputBlank = "This field is required";
-
 const calculateAge = () => {
   const currentDate = new Date();
   const birthDate = new Date(
@@ -29,7 +28,6 @@ const calculateAge = () => {
     Number(inputMonth.value) < 1 ||
     Number(inputYear.value) >= currentDate.getFullYear()
   ) {
-  if(birthDate.getDate()!==Number(inputDay.value)||Number(inputMonth.value)>12||Number(inputMonth.value)<1||Number(inputYear.value)>currentDate.getFullYear()){
     inputDay.classList.add("error");
     labelDay.classList.add("error");
     inputMonth.classList.add("error");
@@ -42,21 +40,21 @@ const calculateAge = () => {
     if (birthDate.getDate() !== Number(inputDay.value)) {
       inputErrorMessageDay.innerText = "Must be a valid day";
       inputErrorMessageDay.style.visibility = "visible";
-    }else{
+    } else {
       inputErrorMessageDay.style.visibility = "hidden";
     }
     if (Number(inputMonth.value) > 12 || Number(inputMonth.value) < 1) {
       console.log("it run this code");
       inputErrorMessageMonth.innerText = "Must be a valid Month";
       inputErrorMessageMonth.style.visibility = "visible";
-    }else{
-      inputErrorMessageMonth.style.visibility ="hidden";
+    } else {
+      inputErrorMessageMonth.style.visibility = "hidden";
     }
     if (Number(inputYear.value) >= currentDate.getFullYear()) {
       inputErrorMessageYear.innerText = "Must be in the past";
       inputErrorMessageYear.style.visibility = "visible";
-    }else{
-      inputErrorMessageYear.style.visibility ="hidden";
+    } else {
+      inputErrorMessageYear.style.visibility = "hidden";
     }
   } else {
     inputDay.classList.remove("error");
@@ -72,28 +70,6 @@ const calculateAge = () => {
       years--;
       months += 12;
     }
-=======
-    if(birthDate.getDate()!==Number(inputDay.value)){
-      inputErrorMessageDay.innerText = "Must be a valid day";
-      inputErrorMessageDay.style.visibility = "visible";
-    }
-    if(Number(inputMonth.value)>12||Number(inputMonth.value)<1){
-      console.log("it run this code");
-      inputErrorMessageMonth.innerText = "Must be a valid Month"
-      inputErrorMessageMonth.style.visibility = "visible";
-    }
-    if(Number(inputYear.value)>=currentDate.getFullYear()){
-      inputErrorMessageYear.innerText ="Must be in the past";
-      inputErrorMessageYear.style.visibility = "visible"
-    }
-  }
-  let years = currentDate.getFullYear() - birthDate.getFullYear();
-  let months = currentDate.getMonth() - birthDate.getMonth();
-  let days = currentDate.getDate() - birthDate.getDate();
-  if (months < 0 || (months == 0 && days < 0)) {
-    years--;
-    months += 12;
-  }
 
     if (days < 0) {
       months--;
@@ -108,8 +84,8 @@ const calculateAge = () => {
     showResultMonths.textContent = months;
     showResultDays.textContent = days;
     inputErrorMessageDay.style.visibility = "hidden";
-    inputErrorMessageMonth.style.visibility ="hidden";
-    inputErrorMessageYear.style.visibility ="hidden";
+    inputErrorMessageMonth.style.visibility = "hidden";
+    inputErrorMessageYear.style.visibility = "hidden";
   }
 };
 
@@ -147,45 +123,4 @@ const showResult = () => {
     }
   }
 };
-
-  showResultYears.textContent = years;
-  showResultMonths.textContent = months;
-  showResultDays.textContent = days;
-};
-
-const showResult = () => {
-  if (
-    inputDay.value !== "" &&
-    inputMonth.value !== "" &&
-    inputYear.value !== ""
-  ) {
-    calculateAge();
-  } else {
-    inputDay.classList.add("error");
-    labelDay.classList.add("error");
-    inputMonth.classList.add("error");
-    labelMonth.classList.add("error");
-    inputYear.classList.add("error");
-    labelYear.classList.add("error");
-    if (inputDay.value === "") {
-      inputErrorMessageDay.innerText = errorMessageInputBlank;
-      inputErrorMessageDay.style.visibility = "visible";
-    } else {
-      inputErrorMessageDay.style.visibility = "hidden";
-    }
-    if (inputMonth.value === "") {
-      inputErrorMessageMonth.innerText = errorMessageInputBlank;
-      inputErrorMessageMonth.style.visibility = "visible";
-    } else {
-      inputErrorMessageMonth.style.visibility = "hidden";
-    }
-    if (inputYear.value === "") {
-      inputErrorMessageYear.innerText = errorMessageInputBlank; 
-      inputErrorMessageYear.style.visibility = "visible";
-    } else {
-      inputErrorMessageYear.style.visibility = "hidden";
-    }
-  }
-};
-
 btn.addEventListener("click", showResult);
